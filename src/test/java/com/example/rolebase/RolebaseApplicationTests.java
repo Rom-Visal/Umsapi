0d4afa05c16cda47d13060ae60faa0accecced28
Rom-Visal <salbongvi@gmail.com>
2026-03-03 14:30:13 +0700

    test(repository): add Data JPA tests for user and role constraints
    Signed-off-by: Rom-Visal <salbongvi@gmail.com>

--- src/test/java/com/example/rolebase/RolebaseApplicationTests.java
+++ src/test/java/com/example/rolebase/RolebaseApplicationTests.java
@@ -2,8 +2,10 @@ package com.example.rolebase;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
+@ActiveProfiles("test")
 class RolebaseApplicationTests {
 
     @Test
